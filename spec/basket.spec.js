87678540a37d15bc13100ecdcb24de308a0e325b
Kiran Gurung <kg820532@gmail.com>
2022-01-14 17:48:55 +0000

    WIP

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -5,30 +5,46 @@ describe("Basket", () => {
     beforeEach(() => {
         basket = new Basket();
     });
-   
-//Test 1
+
+    //Test 1
     it("get all basket", () => {
         const expected = []
         let getBasket = basket.getBasket()
         expect(getBasket).toEqual(expected)
     })
 
-//Test 2
-   it ("add bagel to basket", () => {
-        const expected = ["bagel"]
+    //Test 2
+    it("add items to basket", () => {
+        const expected = [{ SKU: 1, item: "Bagel", quantity: 1 }]
+       // { item: "Brownie", quantity: 3 }]
 
-        let bagelInBasket = basket.addItem("bagel")
+        basket.addItem( "Bagel",  1 )
+        //basket.addItem( "Brownie", 3 )
+
+        let bagelInBasket = basket.getBasket()
         expect(bagelInBasket).toEqual(expected)
     })
 
-    //Test 3
-    it ("remove bagel from basket", () => {
-        const expected = []
+  /*  //Test 3
+    it("remove bagel from basket", () => {
+        const expected = [{ SKU: 1, item: "Bagel", quantity: 1}]
 
-        let removeItem = basket.removeItem("bagel")
+        basket.addItem( "Bagel",  1 )
+        basket.addItem( "Brownie",  3 )
+        let removeItem = basket.removeItem( "Brownie", 3 )
         expect(removeItem).toEqual(expected)
     })
 
+    //Test 4
+    it(" alert when basket is full", () => {
+        const expected = "Basket full, Please choose a bigger basket"
+
+        basket.addItem({ item: "Bagel", quantity: 3 })
+        basket.addItem({ item: "Brownie", quantity: 3 })
+        let alert = basket.basketCapacity()
+        expect(alert).toEqual(expected)
+
+    })*/
 
 
 
--- src/basket.js
+++ src/basket.js
@@ -1,26 +1,46 @@
+const smallBasket = 5;
+const mediumBasket = 10;
+const largeBasket = 15;
+
 class Basket {
 
-    constructor() {
+    constructor(capacity = smallBasket) {
         this.basket = []
+        this.counter = 1
+        this.basketSize = capacity
+        //this.quantiy = this.basket.quantiy
+
     }
 
-    getBasket() {
+    getBasket(){
         return this.basket
     }
 
-    addItem(item) {
-        
-        this.basket.push(item) 
-        {return this.basket}
+    addItem(itemName, itemQuantity) {
+            const insideBasket = {
+                SKU : this.counter,
+                item : itemName,
+                quantity : itemQuantity,
+            }
+            this.counter++
+        this.basket.push(insideBasket)
+       //return this.basket
+     console.log("basket", insideBasket)
     }
 
-    removeItem(item){
-        for (let i = 0; i < this.basket.length; i++) 
-        if (this.basket[i]=== item)
-        {this.basket.splice(i)}
-{return this.basket}
+    removeItem(item) {
+        /*for (let i = 0; i < this.basket.length; i++)
+            if (this.basket[i] === item) { this.basket.splice(i) }
+        { return this.basket }*/
+        this.basket.filter(items => items === item)
+
     }
 
+    basketCapacity(){
+        for (let i = 0; i < this.quantity.length; i++) 
+            if (this.quantity.sum([i]) > smallBasket) {return "Basket full, Please choose a bigger basket"}
+
+    }
 
 
 
